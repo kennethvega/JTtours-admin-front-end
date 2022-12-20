8a1a9e925b43505163056989adcecafa08983d3b
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-12-20 10:56:40 +0800

    Redux, tailwind, navbar setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../assets/JT-tours&travels.jpg';
+import { Link } from 'react-router-dom';
+import Button from './utility/Button';
+import { useAppSelector } from '../redux/hooks';
+import { selectIsLoggedIn } from '../redux/features/auth/authSlice';
+const Navbar = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+
+  return (
+    <div className="w-full shadow">
+      <div className="mx-auto w-[80rem] h-16 flex justify-between items-center">
+        <img src={logo} alt="Logo" className="w-16" />
+        {isLoggedIn ? (
+          <Button>Logout</Button>
+        ) : (
+          <div className="flex gap-10 items-center">
+            <Link to="/login">Login</Link>
+            <Link to="/register">
+              <Button>Register</Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,23 @@
+import { RootState } from './../../store';
+import { createSlice } from '@reduxjs/toolkit';
+
+type InitialState = {
+    isLoggedIn: boolean;
+};
+
+const initialState: InitialState = {
+    isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+});
+
+// ACTIONS
+export const {} = authSlice.actions;
+// STATE
+export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn;
+
+export default authSlice.reducer;
